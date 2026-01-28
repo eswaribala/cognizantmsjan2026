45ba45419fd7b7a8bcc49956df48e01142abc9d9
Parameswari Ettiappan <parameswaribala@gmail.com>
2026-01-28 14:33:21 +0530

    updated

---
+++ hospitalmanagementui/src/main/java/com/cognizant/hospitalmgmt/HospitalmanagementuiApplication.java
@@ -0,0 +1,13 @@
+package com.cognizant.hospitalmgmt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospitalmanagementuiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HospitalmanagementuiApplication.class, args);
+	}
+
+}
