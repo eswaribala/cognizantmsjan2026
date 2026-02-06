d0b61bd11088ea07f27a77237956202ee0cae76a
Parameswari Ettiappan <parameswaribala@gmail.com>
2026-02-06 14:30:36 +0530

    updated

---
+++ productapi/src/main/java/com/cognizant/ecommerce/configurations/VaultConfiguration.java
@@ -0,0 +1,14 @@
+package com.cognizant.ecommerce.configurations;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+import lombok.Data;
+@Data
+@ConfigurationProperties
+public class VaultConfiguration {
+	
+	private String mysqlusername;
+	private String mysqlpassword;
+	
+
+}
