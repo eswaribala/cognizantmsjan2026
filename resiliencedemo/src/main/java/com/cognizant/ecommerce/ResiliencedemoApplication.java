3bd0f9dc9fc3d5de5a961bed1890905249b2a90b
Parameswari Ettiappan <parameswaribala@gmail.com>
2026-02-13 10:25:24 +0530

    updated

---
+++ resiliencedemo/src/main/java/com/cognizant/ecommerce/ResiliencedemoApplication.java
@@ -0,0 +1,13 @@
+package com.cognizant.ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResiliencedemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResiliencedemoApplication.class, args);
+	}
+
+}
